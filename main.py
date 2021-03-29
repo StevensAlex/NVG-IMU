91a23564d0bda3886ff80c8bbb48af8191ea0f7f
Alexander Stevens <astev@UG.KTH.SE>
2021-03-29 13:39:03 +0200

    prototyp på steglängd

--- main.py
+++ main.py
@@ -12,6 +12,46 @@ dataArray = subject.LAcalArray
 
 dtimeArray = dataTime[:,0]
 
+timeStart = dataTime[0, 4]*3600 + dataTime[0, 5]*60 + dataTime[0, 6]
+last = len(dataTime[:,0]) - 1
+timeStop = dataTime[last, 4]*3600 + dataTime[last, 5]*60 + dataTime[last, 6]
+timeTotal = timeStop - timeStart
+print(timeTotal)
+dt = timeTotal / len(dataArray[:,0])
+print("time per packet: ", dt, 'implying data rate of ', 1/dt , 'Hz')
+globalTimeArr = np.linspace(0, timeTotal, num=len(dataArray[:,0]))
+
+a_x = []
+a_z = []
+theta = [dataArray[0, 2]*dt]
+for i in range(len(dataArray[:,0])):
+    dTheta = dataArray[i, 2]
+    dThetaPrev = dataArray[i-1, 2]
+    theta = (dTheta*dt*i) - (dThetaPrev*dt*(i-1))
+    mat = np.array([[-(math.sin(theta)), math.cos(theta)],[math.cos(theta), math.sin(theta)]])
+    vec = np.array([[dataArray[i,4]*9.80665],[dataArray[i,6]*9.80665]])
+    product = np.dot(mat, vec)
+    accVec = (-product) - np.array([[0],[9.80665]])
+    a_x.append(accVec[0,0])
+    a_z.append(accVec[1,0])
+
+xList = [(a_x[0]*dt**2)/2]
+zList = [(a_z[0]*dt**2)/2]
+x=0
+z=0
+for i in range(len(a_x) - 1):
+    x += ((a_x[i+1]*(dt**2))/2) - ((a_x[i]*(dt**2))/2)
+    z += ((a_z[i+1]*(dt**2))/2) - ((a_z[i]*(dt**2))/2)
+    xList.append(x)
+    zList.append(z)
+
+
+plt.figure()
+plt.plot(globalTimeArr, zList)
+plt.title("horizontal foot movement")
+plt.show()
+
+
 def switch_dataRate(argument):
     switcher = {
         0: "Disabled",
@@ -119,6 +159,7 @@ print('During '+ str(fqArray[len(stpFreq)]-fqArray[0]) + ' s in the interval of 
 print('Sample standard diviation: ' + str(stat.stdev(stpFreq)))             #Sample standard deviation of data
 
     #Plotting:
+plt.figure()
 for i in range(0,len(fqArray)):
     plt.axvline(fqArray[i], color = 'r', ymin= 0.15, ymax=0.85)
 #plt.plot(dataArray[:,0], dataArray[:,4], label="X")                        #Print whole dataArray
