c00b46f78ddf714ab3e1635b4ab3b97fc98740fc
Henrik Malmberg <59482405+Henkmeizter@users.noreply.github.com>
2021-05-18 09:47:16 +0200

    minor funcional update

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ import numpy as np
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import filedialog
+from tkinter.ttk import Progressbar
 import arrayImporter as imp
 import arrayHandler as arrH
 import calculations as calc
@@ -10,6 +11,7 @@ from matplotlib.widgets import LassoSelector
 from mpl_toolkits.mplot3d import axes3d
 import statistics as stat                   #temporärt i GUI
 import csv
+import threading
 
 class GUI:
 
@@ -17,50 +19,35 @@ class GUI:
         window.title("xIMU analysis")
         window.minsize(400,600)
         #window.geometry()
-
+        
         self.stepsText = tk.StringVar(window)
         self.frequencyText = tk.StringVar(window)
         self.heightText1 = tk.StringVar(window)
         self.heightText2 = tk.StringVar(window)
         self.lengthText = tk.StringVar(window)
-        self.loading_label = tk.StringVar()
+        self.loading_text = tk.StringVar()
         self.stepsLabel = tk.Label(window, textvariable=self.stepsText, font=("Arial", 11)).place(x=35,y=150)
         self.frequencyLabel = tk.Label(window,textvariable=self.frequencyText,font=("Arial", 11)).place(x=35,y=170)
         self.heightLabel1 = tk.Label(window,textvariable=self.heightText1,font=("Arial", 11)).place(x=35,y=230)
         self.heightLabel2 = tk.Label(window,textvariable=self.heightText2,font=("Arial", 11)).place(x=35,y=250)
         self.lengthLabel = tk.Label(window,textvariable=self.lengthText,font=("Arial", 11)).place(x=35,y=310)
-        
-        tk.Button(
-            window,
-            text="Välj fil",
-            bg='gray',
-            fg='black',
-            command= self.enterData
-            ).place(x=50, y=17)
 
-        tk.Label(window, textvariable=self.loading_label,fg='green',font=("Arial", 8)).place(x=42,y=47)
+        self.progress = Progressbar(window,orient='horizontal',length=100, mode='indeterminate')
+        self.fileimp_btn = tk.Button(window, text="Välj fil", bg='gray', fg='black', command= self.enterData)
+        self.fileimp_btn.place(x=50, y=17)
+        self.loading_label = tk.Label(window, textvariable=self.loading_text,fg='green',font=("Arial", 8))
 
-        tk.Button(
-            window,
-            text="Välj betingelse",
-            bg='gray',
-            fg='black',
-            command=self.enterBet
-            ).place(x=165, y=17)
+        self.betsel_btn = tk.Button(window, text="Välj betingelse", bg='gray', fg='black', command=self.enterBet)
+        self.betsel_btn.place(x=165, y=17)
 
-        tk.Label(text="Tidsinterval:",font=("Arial", 10)).place(x=30, y=65)
+        tk.Label(text="Tidsinterval:",font=("Arial", 10)).place(x=30, y=67)
         self.t1String = tk.StringVar(window)
         self.t2String = tk.StringVar(window)
-        self.t1 = tk.Entry(window, textvariable=self.t1String, width=6, bg='#FDF0CC', font=("Arial", 11)).place(x=35, y=85)          #change to beige-gray instead of yellow
-        self.t2 = tk.Entry(window, textvariable=self.t2String, width=6, bg='#FDF0CC', font=("Arial", 11)).place(x=90, y=85)
+        self.t1 = tk.Entry(window, textvariable=self.t1String, width=6, bg='#FDF0CC', font=("Arial", 11)).place(x=35, y=88)          #change to beige-gray instead of yellow
+        self.t2 = tk.Entry(window, textvariable=self.t2String, width=6, bg='#FDF0CC', font=("Arial", 11)).place(x=90, y=88)
         
-        tk.Button(
-            window,
-            text="Kör!",
-            bg='green',
-            fg='white',
-            command=self.runCalc          
-            ).place(x=180, y=80)
+        self.makecalc_btn = tk.Button(window, text="Kör!", bg='green', fg='white', command=self.runCalc)
+        self.makecalc_btn.place(x=180, y=85)
         
         tk.Label(text="Stegfrekvens:",font=("Arial", 15)).place(x=30, y=120)        
         tk.Label(text="Steghöjd:",font=("Arial", 15)).place(x=30, y=200)
@@ -68,46 +55,23 @@ class GUI:
         #tk.Label(text="Sidostegsvariation:",font=("Arial", 15)).place(x = 30, y = 340)
         tk.Label(text="Se grafer:",font=("Arial", 15)).place(x=30, y=400)
 
-        tk.Button(
-            window,
-            text="2D",
-            bg='gray',
-            fg='black',
-            command=self.plot2d
-            ).place(x=60, y=440)
-        tk.Button(
-            window,
-            text="3D",
-            bg='gray',
-            fg='black',
-            command=self.plot3d
-            ).place(x=160, y=440)
+        tk.Button(window, text="2D", bg='gray', fg='black', command=self.plot2d).place(x=60, y=440)
+        tk.Button(window, text="3D", bg='gray', fg='black', command=self.plot3d).place(x=160, y=440)
 
-        tk.Button(
-            window,
-            text="-",
-            fg='black',
-            command=self.plotPrevious
-            ).place(x=230,y=440)
-        
+        self.prevplot_btn = tk.Button(window, text="-", fg='black', command=self.plotPrevious)
+        self.prevplot_btn.place(x=230,y=440)
         self.gaitNumber = tk.IntVar(window)
-        tk.Entry(window, textvariable=self.gaitNumber, width=4, state="disabled", font=("Arial", 11)).place(x=245, y=442)
-        tk.Button(
-            window,
-            text="+",
-            fg='black',
-            command=self.plotNext
-            ).place(x=275,y=440)
+        tk.Entry(window, textvariable=self.gaitNumber, width=4, font=("Arial", 11)).place(x=245, y=442)
+        self.nxtplot_btn = tk.Button(window, text="+", fg='black', command=self.plotNext)
+        self.nxtplot_btn.place(x=275,y=440)
         
         tk.Label(text="Lägg till kommentar:",font=("Arial", 11)).place(x = 30, y = 500)
         self.comment = tk.StringVar(window)
         self.entry = tk.Entry(window,textvariable=self.comment, width=31, borderwidth=5, font=("Arial", 12)).place(x=35, y=525)
 
-        tk.Button(
-            window,
-            text="Spara",
-            command=self.saveToFile
-            ).place(x=340, y=525)      
+        self.save_filename = ''
+        self.saveas_btn = tk.Button(window, text="Spara som", command=self.saveAsFile).place(x=337, y=525)
+        self.save_btn = tk.Button(window, text="Spara", command=self.saveToFile).place(x=337, y=555)
         
         self.resetWindow()
 
@@ -147,17 +111,32 @@ class GUI:
         self.lengthText.set('Medellängd ' + f"{str(stp_le)+ ' m':<18}" + '  Stdv ' + f"{str(stdv_le)+ ' m':<18}") 
 
     def enterData(self):
-        #try:
-            self.loading_label.set('')
-            self.subject = imp.Subject()
-            self.dataArrays = arrH.DataArrays()
-            self.dataArrays.setArrays(self.subject)
-            data_name = self.dataArrays.getArray("fileName").split('/')        #funkar
-            self.loading_label.set(str(data_name[len(data_name)-1]) +' laddat!')
-            self.resetWindow()
-            messagebox.showinfo("Notification","Datat har laddat färdigt!")
-        #except:
-            #messagebox.showerror("Notification","Datat kunde inte laddas! \nFörsök igen!")
+        def importing():
+            try:
+                self.loading_label.place_forget()
+                self.subject = imp.Subject()
+                self.dataArrays = arrH.DataArrays()
+                self.dataArrays.setArrays(self.subject)
+                data_name = self.dataArrays.getArray("fileName").split('/')        #funkar
+                self.loading_text.set(str(data_name[len(data_name)-1]) +' laddat!')
+                self.loading_label.place(x=42,y=47)
+                self.resetWindow()
+                messagebox.showinfo("Notification","Datat har laddat färdigt!")
+            except:
+                messagebox.showerror("Notification","Datat kunde inte laddas! \nFörsök igen!")
+            finally:
+                self.progress.stop()
+                self.progress.place_forget()
+                self.fileimp_btn['state']='normal'
+                self.betsel_btn['state'] = 'normal'
+                self.makecalc_btn['state']= 'normal'
+
+        self.fileimp_btn['state'] ='disabled'
+        self.betsel_btn['state'] = 'disabled'
+        self.makecalc_btn['state']= 'disabled'
+        self.progress.place(x=30, y=45)
+        self.progress.start()
+        threading.Thread(target=importing).start()
 
     def enterBet(self):
         try:
@@ -222,12 +201,12 @@ class GUI:
                         self.max_t_val = float(self.t2String.get())
                     else:
                         messagebox.showwarning("Notification", "Ingen data detekterades, \nvänligen försök igen!")
-                        self.t1String.set(' ')
-                        self.t2String.set(' ')
+                        self.t1String.set('')
+                        self.t2String.set('')
                 else:
                     messagebox.showwarning("Notification", "Ingen korrekt markering av data gjordes!")
-                    self.t1String.set(' ')
-                    self.t2String.set(' ')
+                    self.t1String.set('')
+                    self.t2String.set('')
    
     def runCalc(self):
         try:
@@ -269,39 +248,47 @@ class GUI:
         plt.show()
 
     def plotPrevious(self):
-        self.gaitNumber.set(self.gaitNumber.get()-1)
-        print("-")
+        self.gaitNumber.set(self.gaitNumber.get()-1)        
 
     def plotNext(self):
         self.gaitNumber.set(self.gaitNumber.get()+1)
-        print("+")
+
+    def saveAsFile(self):
+        try:
+            self.save_filename = filedialog.asksaveasfilename(title="Save data",filetypes=(("csv files", "*csv"),("all files", "")))
+        except:
+            self.save_filename = ''
+            messagebox.showwarning("Notification", "Något hände!")
+        else:
+            if self.save_filename.endswith(".csv"):
+                pass
+            else:
+                self.save_filename = f'{self.save_filename}.csv'
+            self.saveToFile()
 
     def saveToFile(self):
         try:
-            data_name = self.dataArrays.getArray("fileName").split('/')       #funkar
-            data_name = data_name[len(data_name)-1]
-            file_name = filedialog.asksaveasfilename(title="Save data",filetypes=(("csv files", "*csv"),("all files", "")))
-            if file_name:
-                if file_name.endswith(".csv"):
-                    pass
-                else:
-                    file_name = f'{file_name}.csv'
-
-            csv.register_dialect('myDialect', delimiter=',', quoting=csv.QUOTE_ALL)
-            with open(file_name, 'a',  newline='') as myFile:
-                writer = csv.writer(myFile, dialect='myDialect')
-                myFields = ['Filnamn', 'Startsek', 'Stopsek', 'Antal steg', 'Stegfrekvens', 'Stdv stegfrekvens', 'Medelhöjd', 'Stdv steghöjd',
-                            'Maxhöjd', 'Minhöjd', 'Steglängd', 'Stdv steglängd', 'Kommentar']
+            print(self.save_filename)
+            if self.save_filename == '':
+                self.saveAsFile()
+            else:
+                data_name = self.dataArrays.getArray("fileName").split('/')       #funkar
+                data_name = data_name[len(data_name)-1]
                 
-                writer = csv.DictWriter(myFile, fieldnames=myFields)
-                self.header_check = np.genfromtxt(file_name, delimiter=',')
-                if( len(self.header_check) == 0 ):                          #Assumes that if the file isn't new that it already have correct header
-                    writer.writeheader()
+                csv.register_dialect('myDialect', delimiter=',', quoting=csv.QUOTE_ALL)
+                with open(self.save_filename, 'a',  newline='') as myFile:
+                    myFields = ['Filnamn', 'Startsek', 'Stopsek', 'Antal steg', 'Stegfrekvens', 'Stdv stegfrekvens', 'Medelhöjd', 'Stdv steghöjd',
+                                'Maxhöjd', 'Minhöjd', 'Steglängd', 'Stdv steglängd', 'Kommentar']
                 
-                writer.writerow({'Filnamn': data_name,'Startsek': self.timeStart, 'Stopsek': self.timeStop, 'Antal steg': self.total_steps, 
-                                'Stegfrekvens':self.step_frequency, 'Stdv stegfrekvens':self.stdv_steps, 'Medelhöjd':self.step_height, 'Stdv steghöjd':self.stdv_height, 
-                                'Maxhöjd':self.max_height, 'Minhöjd':self.min_height, 'Steglängd':self.step_length, 'Stdv steglängd':self.stdv_length, 
-                                   'Kommentar':self.comment.get()})
+                    writer = csv.DictWriter(myFile, fieldnames=myFields,dialect='myDialect')
+                    header_check = np.genfromtxt(self.save_filename, delimiter=',')
+                    if( len(header_check) == 0 ):                          #Assumes that if the file isn't new that it already have correct header
+                        writer.writeheader()
+                
+                    writer.writerow({'Filnamn': data_name,'Startsek': self.timeStart, 'Stopsek': self.timeStop, 'Antal steg': self.total_steps, 
+                        'Stegfrekvens':self.step_frequency, 'Stdv stegfrekvens':self.stdv_steps, 'Medelhöjd':self.step_height, 'Stdv steghöjd':self.stdv_height, 
+                        'Maxhöjd':self.max_height, 'Minhöjd':self.min_height, 'Steglängd':self.step_length, 'Stdv steglängd':self.stdv_length, 
+                        'Kommentar':self.comment.get()})
         except:
             messagebox.showwarning("Notification", "Saknas tillräckligt med information! \nHar beräkning genomförts?")
 
